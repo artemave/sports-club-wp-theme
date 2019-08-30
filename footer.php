556c73b9c2ecbbeb12429af63f1bd113a00a52bd
artemave <artemave@gmail.com>
2019-08-30 18:43:47 +0200

    Original theme

---
+++ footer.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Sports Club Lite
+ */
+?>
+
+<div class="footer-wrapper"> 
+      <div class="container">           
+          <?php if ( is_active_sidebar( 'site-footer-widget-column-1' ) ) : ?>
+                <div class="widget-column-1">  
+                    <?php dynamic_sidebar( 'site-footer-widget-column-1' ); ?>
+                </div>
+           <?php endif; ?>
+          
+          <?php if ( is_active_sidebar( 'site-footer-widget-column-2' ) ) : ?>
+                <div class="widget-column-2">  
+                    <?php dynamic_sidebar( 'site-footer-widget-column-2' ); ?>
+                </div>
+           <?php endif; ?>
+           
+           <?php if ( is_active_sidebar( 'site-footer-widget-column-3' ) ) : ?>
+                <div class="widget-column-3">  
+                    <?php dynamic_sidebar( 'site-footer-widget-column-3' ); ?>
+                </div>
+           <?php endif; ?>
+           
+           <?php if ( is_active_sidebar( 'site-footer-widget-column-4' ) ) : ?>
+                <div class="widget-column-4">  
+                    <?php dynamic_sidebar( 'site-footer-widget-column-4' ); ?>
+                </div>
+           <?php endif; ?>
+           
+           <div class="clear"></div>
+      </div><!--end .container-->
+
+        <div class="footer_bottom"> 
+            <div class="container">
+                <div class="wp_powerd_by">
+				  <?php bloginfo('name'); ?> | 
+                  <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sports-club-lite' ) ); ?>">
+				     <?php
+				         /* translators: %s: WordPress. */
+				          printf( __( 'Proudly Powered by %s.', 'sports-club-lite' ), 'WordPress' );
+				     ?>
+			       </a>                 
+                </div>
+                        	
+                <div class="gt_design_by">				
+                  <a href="<?php echo esc_url( __( 'https://gracethemes.com/themes/free-sports-league-wordpress-theme/', 'sports-club-lite' ) ); ?>" target="_blank">
+				    <?php printf( __( 'Theme by %s', 'sports-club-lite' ), 'Grace Themes' ); ?>
+                  </a>
+                </div>
+                <div class="clear"></div>
+             </div><!--end .container-->             
+        </div><!--end .footer_bottom-->  
+                     
+     </div><!--end #footer-wrapper-->
+</div><!--#end layout_forsite-->
+
+<?php wp_footer(); ?>
+</body>
+</html>
